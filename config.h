b863d926f8869ca2b56b606a97b8f1873cee7e8b
J0rdyV <28193980+J0rdyV@users.noreply.github.com>
2022-05-10 09:23:59 +0200

    Patched for center, and edited defaults

--- config.h
+++ config.h
@@ -2,19 +2,23 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
+static const char *fonts[]          = {
+	"Cascadia Code PL:style=Regular:size=11:antialias=true:autohint=true",
+	"Noto Emoji:size=11",
+	"monospace:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#ebdbb2", "#282828" },
+	[SchemeSel] = { "#ebdbb2", "#458588" },
+	[SchemeOut] = { "#ebdbb2", "#458588" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 15;
 
 /*
  * Characters not considered part of a word while deleting words
