9f1ccc51ce99c1f18898c83b7be7d736dfbb6ec9
J0rdyV <28193980+J0rdyV@users.noreply.github.com>
2026-09-16 23:05:10 +0200

    update

--- config.h
+++ config.h
@@ -2,12 +2,14 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[]          = {
-	"Cascadia Code:style=regular:pixelsize=18:antialias=true:autohint=true",
-	/* "Terminus:size=14:antialias=true:autohint=true", */
+	//"Cascadia Code:style=regular:pixelsize=18:antialias=true:autohint=true",
+	"Cascadia Code:style=SemiLight:size=12:antialias=true:autohint=true",
+	//"Terminus:size=14:antialias=true:autohint=true",
 	/* "Cascadia Code PL:style=Regular:size=11:antialias=true:autohint=true", */
 	/* "Misc Tamsyn:style=Regular:pixelsize=15", */
 	"Noto Emoji:size=11",
@@ -15,6 +17,7 @@ static const char *fonts[]          = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
+/* static const char col_black[]       = "#282828"; */
 static const char col_black[]       = "#32302f";
 /* static const char col_white[]       = "#d7d7d7"; */
 
@@ -24,10 +27,19 @@ static const char col_black[]       = "#32302f";
 static const char col_white[]       = "#ebdbb2";
 /* static const char col_orange[]      = "#fabd2f"; */
 
+static const char col_gray[]       = "#928374";
+static const char col_red[]        = "#cc241d";
+static const char col_green[]      = "#b8bb26";
+static const char col_yellow[]     = "#fabd2f";
+static const char col_blue[]       = "#83a598";
+static const char col_purple[]     = "#d3869b";
+static const char col_aqua[]       = "#8ec07c";
+static const char col_orange[]     = "#fe8019";
+
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { col_white, col_black },
+	[SchemeNorm] = { col_yellow, col_black },
 	[SchemeSel] = { col_black, col_white },
 	[SchemeOut] = { col_white, col_black },
 };
